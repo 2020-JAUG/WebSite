a6db79e0da0b808ce6d5e8d95309442a8f86c2a5
Alejandro Urbina <urbinagonzalezja@gmail.com>
2024-01-08 23:16:29 +0100

    Create aboutSection

--- app/components/AboutSection.jsx
+++ app/components/AboutSection.jsx
@@ -56,12 +56,10 @@ export const AboutSection = () => {
     }
 
 
-
-
     return (
         <section className=''>
             <div className='md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16'>
-                <Image src='/images/heros.png' width={500} height={500} />
+                <Image src='/images/heros.png' width={500} height={500}/>
                 <div className='mt-4 md:mt-0 text-left flex flex-col h-full'>
                     <h2 className='text-4xl font-bold mb-4'>About Me</h2>
                     <p className='text-base lg:text-lg'>
