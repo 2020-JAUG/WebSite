4a6681eee08e3f1381e58667b4f0a90de267e9b6
Alejandro Urbina <urbinagonzalezja@gmail.com>
2024-01-10 23:56:41 +0100

    Contiue with projectSection

--- app/components/ProjectsSection.jsx
+++ app/components/ProjectsSection.jsx
@@ -1,11 +1,86 @@
 import React from 'react'
+import ProjectCard from './ProjectCard'
 
+
+const projectsData = [
+    {
+        id: 1,
+        title: 'Project 1',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+        imageUrl: 'https://picsum.photos/id/300/200/300',
+        tag: ['All', 'Web'],
+        gitUrl: '/',
+        previewUrl: '/'
+    },
+    {
+        id: 2,
+        title: 'Project 2',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+        imageUrl: 'https://picsum.photos/id/202/200/300',
+        tag: ['All', 'Web'],
+        gitUrl: '/',
+        previewUrl: '/'
+    },
+    {
+        id: 3,
+        title: 'Project 3',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+        imageUrl: 'https://picsum.photos/id/35/200/300',
+        tag: ['All', 'Web'],
+        gitUrl: '/',
+        previewUrl: '/'
+    },
+    {
+        id: 4,
+        title: 'Project 4',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+        imageUrl: 'https://picsum.photos/id/99/200/300',
+        tag: ['All', 'Web'],
+        gitUrl: '/',
+        previewUrl: '/'
+    },
+    {
+        id: 5,
+        title: 'Project 5',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+        imageUrl: 'https://picsum.photos/id/12/200/300',
+        tag: ['All', 'Web'],
+        gitUrl: '/',
+        previewUrl: '/'
+    },
+    {
+        id: 6,
+        title: 'Project 6',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+        imageUrl: 'https://picsum.photos/id/3/200/300',
+        tag: ['All', 'Web'],
+        gitUrl: '/',
+        previewUrl: '/'
+    }
+];
 
 const ProjectsSection = () => {
 
     return (
-        <><h2>My Projects</h2>
-    <div>ProjectsSection</div></>
+        <><h2 className='text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12'>My Projects</h2>
+            <div className='text-white flex flex-row justify-center items-center gap-2 py-6'>
+                <button>All</button>
+                <button>Web</button>
+            </div>
+            <div className='grid md:grid-cols-3 gap-8 md:gap-12'>
+                {projectsData.map((project) => (
+                    <ProjectCard
+                        key={project.id}
+                        title={project.title}
+                        description={project.description}
+                        imageUrl={project.imageUrl}
+                        tag={project.tag}
+                        gitUrl={project.gitUrl}
+                        previewUrl={project.previewUrl}
+                    />
+                ))}
+            </div>
+        </>
     )
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,18 @@
-import { HeroSection } from '@/app/components/HeroSection'
-import { Navbar } from '@/app/components/NavBar'
-import { AboutSection } from '@/app/components/AboutSection'
+import {HeroSection} from '@/app/components/HeroSection'
+import {Navbar} from '@/app/components/NavBar'
+import {AboutSection} from '@/app/components/AboutSection'
+import ProjectsSection from "@/app/components/ProjectsSection";
 
 export default function Home() {
 
-  return (
-    <main className='flex min-h-screen flex-col bg-[#121212]'>
-      <Navbar />
-      <div className='container mt-24 mx-auto px-12 py-4'>
-        <HeroSection />
-        <AboutSection />
-      </div>
-    </main>
-  )
+    return (
+        <main className='flex min-h-screen flex-col bg-[#121212]'>
+            <Navbar/>
+            <div className='container mt-24 mx-auto px-12 py-4'>
+                <HeroSection/>
+                <AboutSection/>
+                <ProjectsSection/>
+            </div>
+        </main>
+    )
 }
