c2253d801f65145299e31671782c6fa0a26916a7
Alejandro Urbina <urbinagonzalezja@gmail.com>
2024-02-12 21:58:54 +0100

    Refactor AboutSection component and update tab handling

--- app/components/AboutSection.jsx
+++ app/components/AboutSection.jsx
@@ -4,52 +4,52 @@ import React, { useTransition, useState } from 'react'
 import { TabButton } from '@/app/components/TabButton'
 
 
+const TAB_DATA = [
+    {
+        title: 'Skills',
+        id: 'skills',
+        content: (
+            <ul className='list-disc pl-2'>
+                <li>JavaScript</li>
+                <li>PHP</li>
+                <li>MySQL</li>
+                <li>React</li>
+                <li>NodeJS</li>
+                <li>TypeORM</li>
+                <li>Laravel</li>
+                <li>Docker</li>
+            </ul>
+        )
+    },
+    {
+        title: 'Certifications',
+        id: 'certifications',
+        content: (
+            <ul className='list-disc pl-2'>
+                <li>TypeScript Udemy</li>
+                <li>Docker Udemy</li>
+                <li>Object oriented programming in Php Platzi</li>
+            </ul>
+        )
+    },
+    {
+        title: 'Education',
+        id: 'education',
+        content: (
+            <ul className='list-disc pl-2'>
+                <li>DAW Misericordia</li>
+                <li>Bootmcamp FullStack</li>
+            </ul>
+        )
+    }
+]
+
 export const AboutSection = () => {
 
     const [tab, setTab] = useState('skills')
-    const [isPending, startTransition] = useTransition()
+    const [isPending, startTransition] = useTransition();
 
-    const TAB_DATA = [
-        {
-            title: 'Skills',
-            id: 'skills',
-            content: (
-                <ul className='list-disc pl-2'>
-                    <li>JavaScript</li>
-                    <li>PHP</li>
-                    <li>MySQL</li>
-                    <li>React</li>
-                    <li>NodeJS</li>
-                    <li>TypeORM</li>
-                    <li>Laravel</li>
-                    <li>Docker</li>
-                </ul>
-            )
-        },
-        {
-            title: 'Certifications',
-            id: 'certifications',
-            content: (
-                <ul className='list-disc pl-2'>
-                    <li>TypeScript Udemy</li>
-                    <li>Docker Udemy</li>
-                    <li>Object oriented programming in Php Platzi</li>
-                </ul>
-            )
-        },
-        {
-            title: 'Education',
-            id: 'education',
-            content: (
-                <ul className='list-disc pl-2'>
-                    <li>DAW Misericordia</li>
-                    <li>Bootmcamp FullStack</li>
-                </ul>
-            )
-        }
-    ]
-
-    const handleTab = (id) => {
+    const handleTabChange = (id) => {
         startTransition(() => {
             setTab(id)
         })
@@ -57,9 +57,9 @@ export const AboutSection = () => {
 
 
     return (
-        <section className=''>
+        <section className='text-white' id='about'>
             <div className='md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16'>
-                <Image src='/images/heros.png' width={500} height={500}/>
+                <Image src='/images/heros.png' alt='heros' width={500} height={500} />
                 <div className='mt-4 md:mt-0 text-left flex flex-col h-full'>
                     <h2 className='text-4xl font-bold mb-4'>About Me</h2>
                     <p className='text-base lg:text-lg'>
@@ -68,20 +68,20 @@ export const AboutSection = () => {
                         I have experience working with: JavaScript, PHP, HTML, MySQL, React, NodeJS, TypeORM, Laravel, Docker. I am a fast learner and always looking to expand my knowledge and skills.
                     </p>
                     <div className='flex flex-row justify-start mt-8'>
-                        <TabButton selectTab={() => handleTab("skills")} active={tab === "skills"}>
+                        <TabButton selectTab={() => handleTabChange("skills")} active={tab === "skills"}>
                             {" "}
                             Skills{" "}
                         </TabButton>
-                        <TabButton selectTab={() => handleTab('certifications')} active={tab === 'certifications'}>
+                        <TabButton selectTab={() => handleTabChange('certifications')} active={tab === 'certifications'}>
                             {" "}
                             Certifications{" "}
                         </TabButton>
-                        <TabButton selectTab={() => handleTab('education')} active={tab === 'education'}>
+                        <TabButton selectTab={() => handleTabChange('education')} active={tab === 'education'}>
                             {" "}
                             Education{" "}
                         </TabButton>
                     </div>
-                    <div className='mt-8'>{ TAB_DATA.find((t) => t.id === tab).content }</div>
+                    <div className='mt-8'>{TAB_DATA.find((t) => t.id === tab).content}</div>
                 </div>
             </div>
         </section>
