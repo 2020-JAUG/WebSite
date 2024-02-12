b3f631d38c0ee5f2cfe24b8823e4fd4fa8869cda
Alejandro Urbina <urbinagonzalezja@gmail.com>
2024-02-12 22:52:08 +0100

    Commented out unused code in send route

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -1,36 +1,36 @@
-import { NextResponse, ServerComponents } from 'next/server';
-//import { EmailTemplate } from '../../components/EmailTemplate';
-import { Resend } from 'resend';
+// import { NextResponse, ServerComponents } from 'next/server';
+// //import { EmailTemplate } from '../../components/EmailTemplate';
+// import { Resend } from 'resend';
 
-const { ServerComponents: { Fragment } } = ServerComponents;
+// const { ServerComponents: { Fragment } } = ServerComponents;
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-const fromEmail = process.env.FROM_EMAIL;
+// const resend = new Resend(process.env.RESEND_API_KEY);
+// const fromEmail = process.env.FROM_EMAIL;
 
-export async function POST(req, res) {
+// export async function POST(req, res) {
 
-    const { body } = req.json();
-    const { email, subject, message } = body;
+//     const { body } = req.json();
+//     const { email, subject, message } = body;
 
-    try {
+//     try {
 
-        const { data, error } = await resend.emails.send({
-            from: fromEmail,
-            to: [fromEmail, email],
-            subject: subject,
-            //react: EmailTemplate({ firstName: 'John' }),
-            react: (
-                <Fragment>
-                    <h1>{subject}</h1>
-                    <p>Thank you for contacting us!</p>
-                    <p>New message submitted:</p>
-                    <p>{message}</p>
-                </Fragment>
-            )
-        });
-        return NextResponse.json(data);
-    } catch (error) {
-        console.error('Error sending email: ', error);
-        return NextResponse.json({ error });
-    }
-};
+//         const { data, error } = await resend.emails.send({
+//             from: fromEmail,
+//             to: [fromEmail, email],
+//             subject: subject,
+//             //react: EmailTemplate({ firstName: 'John' }),
+//             react: (
+//                 <Fragment>
+//                     <h1>{subject}</h1>
+//                     <p>Thank you for contacting us!</p>
+//                     <p>New message submitted:</p>
+//                     <p>{message}</p>
+//                 </Fragment>
+//             )
+//         });
+//         return NextResponse.json(data);
+//     } catch (error) {
+//         console.error('Error sending email: ', error);
+//         return NextResponse.json({ error });
+//     }
+// };
