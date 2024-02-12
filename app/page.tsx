78688fbbf799f4fc9a3ee624ac92c6723e334f52
Alejandro Urbina <urbinagonzalezja@gmail.com>
2024-02-12 21:57:41 +0100

    Comment footer component

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,21 @@
-import {HeroSection} from '@/app/components/HeroSection'
-import {Navbar} from '@/app/components/NavBar'
-import {AboutSection} from '@/app/components/AboutSection'
-import ProjectsSection from "@/app/components/ProjectsSection";
+import { HeroSection } from '@/app/components/HeroSection';
+import { Navbar } from '@/app/components/NavBar';
+import { AboutSection } from '@/app/components/AboutSection';
+import ProjectsSection from '@/app/components/ProjectsSection';
+import EmailSection from '@/app/components/Emailsection';
+import Footer from '@/app/components/Footer';
 
 export default function Home() {
 
     return (
         <main className='flex min-h-screen flex-col bg-[#121212]'>
-            <Navbar/>
+            <Navbar />
             <div className='container mt-24 mx-auto px-12 py-4'>
-                <HeroSection/>
-                <AboutSection/>
-                <ProjectsSection/>
+                <HeroSection />
+                <AboutSection />
+                <ProjectsSection />
+                <EmailSection />
+                {/* <Footer /> */}
             </div>
         </main>
     )
