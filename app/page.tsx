09e1669a80b5305e7e99a0b1437aa4e7c18e26a2
Alejandro Urbina <urbinagonzalezja@gmail.com>
2024-02-25 20:04:23 +0100

    Uncomment components

--- app/page.tsx
+++ app/page.tsx
@@ -8,15 +8,15 @@ import Footer from '../app/components/Footer';
 export default function Home() {
 
     return (
-        <main className='flex min-h-screen flex-col bg-[#121212]'>
-            <Navbar />
+        <main className='flex min-h-screen flex-col bg-[#121212] w-full'>
+            <Navbar/>
             <div className='container mt-24 mx-auto px-12 py-4'>
-                <HeroSection />
-                <AboutSection />
-                <ProjectsSection />
-                {/* <EmailSection /> */}
-                {/* <Footer /> */}
+                <HeroSection/>
+                <AboutSection/>
+                <ProjectsSection/>
+                <EmailSection/>
             </div>
+            <Footer/>
         </main>
     )
 }
