6bdc37c0fd904b4d1b29e09916568416d8074268
Alejandro Urbina <urbinagonzalezja@gmail.com>
2024-02-25 01:17:41 +0100

    Add url gitHAdd padding at the up test

--- app/components/ProjectsSection.jsx
+++ app/components/ProjectsSection.jsx
@@ -78,7 +78,7 @@ const ProjectsSection = () => {
     });
 
     return (
-        <section id='projects'>
+        <section id='projects' style={{ paddingTop: '60px', marginTop: '-60px' }}>
             <h2 className='text-center text-4xl font-bold text-white mt-8 mb-8 md:mb-12'>My Projects</h2>
             <div className='text-white flex flex-row justify-center items-center gap-2 py-6'>
                 <ProjectTag tag='All' onClick={() => handleTagClick('All')} isSelected={selectedTag === 'All'} />
