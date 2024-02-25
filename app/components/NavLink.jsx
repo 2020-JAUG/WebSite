308da57bfdc2f0e8a5b499b4a71aabb24eb8d163
Alejandro Urbina <urbinagonzalezja@gmail.com>
2024-02-25 21:26:59 +0100

    Remove a of link

--- app/components/NavLink.jsx
+++ app/components/NavLink.jsx
@@ -3,10 +3,12 @@ import Link from 'next/link'
 
 export const NavLink = ({ href, title, onClick }) => {
     return (
-        <Link href={href} onClick={onClick}>
-            <a className='block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white'>
-                <span aria-label={title}>{title}</span>
-            </a>
+        <Link
+            href={href}
+            onClick={onClick}
+            className='block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white'
+        >
+            {title}
         </Link>
     )
 }
