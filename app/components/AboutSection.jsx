d3bb2068db263080f552625ee3228e401fecddc7
Alejandro Urbina <urbinagonzalezja@gmail.com>
2024-03-17 13:10:19 +0100

    change order to list TAB_DATA const

--- app/components/AboutSection.jsx
+++ app/components/AboutSection.jsx
@@ -46,7 +46,7 @@ const TAB_DATA = [
 
 const AboutSection = () => {
 
-    const [tab, setTab] = useState('skills')
+    const [tab, setTab] = useState('certifications')
     const [isPending, startTransition] = useTransition();
 
     const handleTabChange = (id) => {
