001d45125edea1f72e5f90b8a25b2d083738d93e
Alejandro Urbina <urbinagonzalezja@gmail.com>
2024-03-23 15:33:11 +0100

    Fix redirect

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -8,19 +8,19 @@ import { MenuOverlay } from './MenuOverlay'
 const navLinks = [
     {
         title: 'About',
-        path: '#about',
+        path: '/#about',
     },
     {
         title: 'Repositories',
-        path: '#repositories',
+        path: '/#repositories',
     },
     {
         title: 'Labs',
-        path: '#labs',
+        path: '/labs',
     },
     {
         title: 'Contact',
-        path: '#contact',
+        path: '/#contact',
     },
 ];
 
@@ -29,10 +29,10 @@ const Navbar = () => {
     const [navbarOpen, setNavbarOpen] = useState(false);
 
     return (
-        <nav className='fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100'>
+        <nav className='fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-20 bg-[#121212] bg-opacity-100'>
             <div className='flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2'>
                 <Link
-                    href={'/#contact'}
+                    href={'/'}
                     className='text-2xl md:text-5xl text-white font-semibold'
                 >
                     LOGO
