0717c80eaebfd5f96bbb70bb39b754a9aceb1b7f
Alejandro Urbina <urbinagonzalezja@gmail.com>
2024-03-24 20:14:22 +0100

    Update send message button

--- app/components/Emailsection.jsx
+++ app/components/Emailsection.jsx
@@ -1,52 +1,46 @@
-"use client";
+'use client';
 import React, {useState} from 'react';
 import GithubIcon from '../../public/github-icon.svg';
 import LinkedinIcon from '../../public/linkedin-icon.svg';
 import Link from 'next/link';
 import Image from 'next/image';
 
-
 const EmailSection = () => {
-
     const [emailSubmitted, setEmailSubmitted] = useState(false);
+    const [isSending, setIsSending] = useState(false);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+        setIsSending(true);
         const data = {
             email: e.target.email.value,
             subject: e.target.subject.value,
             message: e.target.message.value,
         };
-        const JSONdata = JSON.stringify(data);
+        const JSONData = JSON.stringify(data);
 
-        console.log('who send?  ', JSONdata)
         const endpoint = '/api/send';
-
-        // Form the request for sending data to the server.
         const options = {
-            // The method is POST because we are sending data.
             method: 'POST',
-            // Tell the server we're sending JSON.
             headers: {
                 'Content-Type': 'application/json',
             },
-            // Body of the request is the JSON data we created above.
-            body: JSONdata,
+            body: JSONData,
         };
 
-        // Send the request to the server.
         const response = await fetch(endpoint, options);
-        console.log('response ', response);
-        // Get the response from the server.
-        const responseData = await response.json();
+        // const responseData = await response.json();
 
-        console.log('response ', responseData, ' otro => ', response);
-        // If the response is successful, set emailSubmitted to true.
         if (response.status === 200) {
-            console.log("Message sent.");
+            setIsSending(false);
             setEmailSubmitted(true);
+            e.target.reset();
+            setTimeout(() => {
+                setEmailSubmitted(false);
+            }, 1500);
+        } else {
+            setIsSending(false);
         }
-
     };
 
     return (
@@ -76,11 +70,7 @@ const EmailSection = () => {
                 </div>
             </div>
             <div>
-                {emailSubmitted ? (
-                    <p className='text-green-500 text-sm mt-2'>
-                        Email sent successfully!
-                    </p>
-                ) : (
+                <div>
                     <form className='flex flex-col' onSubmit={handleSubmit}>
                         <div className='mb-6'>
                             <label
@@ -128,21 +118,24 @@ const EmailSection = () => {
                                 placeholder="Let' s talk about..."
                             />
                         </div>
-                        {<button
-                            type='submit'
-                            className='bg-gradient-to-br from-green-500 to-blue-700 text-white font-medium py-2.5 px-5 rounded-lg w-full'
-                        >
-                            Send Message
-                        </button>}
-                        {
-                            emailSubmitted && (
-                                <p className='text-green-500 text-sm mt-2'>
-                                    Email sent successfully!
-                                </p>
-                            )
-                        }
+                        {isSending ? (
+                            <p className='text-green-500 text-sm mt-2'>
+                                Sending email...
+                            </p>
+                        ) : emailSubmitted ? (
+                            <p className='text-green-500 text-sm mt-2'>
+                                Email sent successfully!
+                            </p>
+                        ) : (
+                            <button
+                                type='submit'
+                                className='bg-gradient-to-br from-green-500 to-blue-700 text-white font-medium py-2.5 px-5 rounded-lg w-full'
+                            >
+                                Send Message
+                            </button>
+                        )}
                     </form>
-                )}
+                </div>
             </div>
         </section>
     );
