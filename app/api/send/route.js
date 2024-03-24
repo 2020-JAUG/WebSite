36a7d4b6a24a62296f1096a038813a90670cf864
Alejandro Urbina <urbinagonzalezja@gmail.com>
2024-03-24 20:22:36 +0100

    Add styles to email blade

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -1,12 +1,13 @@
 import { NextResponse } from 'next/server';
 import { Resend } from 'resend';
+import * as React from 'react';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
 
 export async function POST(req, res) {
 
-    const { email, subject, message } = await req.json();
+    const { email, subject } = await req.json();
     try {
         const data = await resend.emails.send({
             from: fromEmail,
@@ -14,12 +15,13 @@ export async function POST(req, res) {
             subject: subject,
             react: (
                 <>
-                    <h1>{subject}</h1>
-                    <p>Thank you for contacting us!</p>
-                    <p>New message submitted:</p>
-                    <p>{message}</p>
+                    <div style={{ backgroundColor: 'black', padding: '20px' }}>
+                        <h5 style={{ color: 'white' }}>Hi there, {email}!</h5>
+                        <p style={{ color: 'white' }}>Thank you for contacting us!</p>
+                        <p style={{ color: 'white' }}>We will get back to you as soon as possible.</p>
+                    </div>
                 </>
-            ),
+            )
         });
         return NextResponse.json(data);
     } catch (error) {
